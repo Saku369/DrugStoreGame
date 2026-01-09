d7328a2ee7093939682b3df6cdcc0e75c6b3a5f0
Saku Nakabayashi <b1023168@fun.ac.jp>
2026-01-09 18:47:55 +0900

    ステージの作成

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -1,23 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rb;
+    [SerializeField]
+    [Tooltip("プレイヤーの速度")]
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+    
     void Start()
     {
-        testInputAction = new TestInputAction();
-        testInputAction.Enable();
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;//キャラが勝手に倒れたりするのを防ぐ
     }
 
-    // Update is called once per frame
-    void Update()
+    // --- 移動入力（WASD / スティック） ---
+    private void OnMove(InputValue value)
     {
-        if (testInputAction.Player.Fire.triggered)
+        moveInput = value.Get<Vector2>(); // ← 入力が0なら自動で Vector2.zero が入る
+    }
+
+    private void FixedUpdate()
+    {
+        // 入力を3D空間に変換
+        Vector3 dir = new Vector3(moveInput.x, 0, moveInput.y);
+
+        // 斜め移動も
+        rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
+        // 入力方向へキャラを向ける
+        if (dir != Vector3.zero)
         {
-            Debug.Log("ファイアー!!!!!");
+            transform.forward = dir.normalized;
+        }
+       
+        if (moveInput != Vector2.zero)
+        {
+            Debug.Log($"Move Input - X: {moveInput.x}, Y: {moveInput.y}");
         }
     }
 }
